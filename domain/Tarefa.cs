acf877aac56c0db8cb7f6b738ff823e765602fce
Bianca Lanzarini <biancalanzarini3@gmail.com>
2025-03-01 13:23:35 -0300

    Commit inicial: estrutura básica do projeto com classes Usuario e Tarefa.

---
+++ domain/Tarefa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace domain
+{
+    class Tarefa
+    {
+        public string Titulo { get; private set; }
+        public string Descricao { get; private set; }
+        public DateTime DataCriacao { get; private set; }
+        public DateTime Prazo { get; private set; }
+        public char Status { get; set; } // farei uma classe ENUM para representar os possíveis status da tarefa
+
+        public Tarefa (string titulo, string descricao, DateTime prazo)
+        {
+            Titulo = titulo;
+            Descricao = descricao;
+            DataCriacao = DateTime.Now.Date;
+            Prazo = prazo;
+            Status = 'P';
+        }
+    }
+}
---
+++ domain/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace domain
+{
+    class Usuario
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Senha { get; private set; }
+
+        public Usuario (string nome, string email, string senha) 
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+        }
+    }
+}
